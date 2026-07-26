3039d98c6d59f935439acf2e793ce454b70db371
JosephAnderson234 <joseph.cose@utec.edu.pe>
2026-07-26 09:01:51 -0500

    feat: fck zod

--- lib/api/schemas/storage.ts
+++ lib/api/schemas/storage.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const fileSchema = z.object({
   id: z.string(),
-  filename: z.string(),
+  key: z.string(),
   originalName: z.string(),
   mimeType: z.string(),
   size: z.number(),
